2468aa5ec1f6facb895687e894d2e4be4880bb2a
YOUR NAME <erinpagemd@yahoo.com>
2015-03-19 23:17:20 -0500

    writing javascript for profile form

--- app/views/profile.js
+++ app/views/profile.js
@@ -2,4 +2,40 @@ angular
 .module('appchiever')
 .controller('ProfileCtrl', ProfileController)
 
-function ProfileController () {};
+function ProfileController ($scope) {
+  getProfileObj($scope);
+};
+
+function getProfileObj ($scope) {
+  var firstname = $scope.firstname;
+  var lastname = $scope.lastname;
+  var schoolname = $scope.schoolname;
+  var schoolmascot = $scope.schoolmascot;
+  var gradyear = $scope.gradyear;
+  var gradcity = $scope.gradcity;
+  var gradstate = $scope.gradstate;
+  var picurl = $scope.picurl;
+  var profileObj = {
+    firstname: firstname,
+    lastname: lastname,
+    schoolname: schoolname,
+    schoolmascot: schoolmascot,
+    gradyear: gradyear,
+    gradcity: gradcity,
+    gradstate: gradstate,
+    picurl: picurl
+  };
+
+  //clear the inputs
+  $scope.firstname = null;
+  $scope.lastname = null;
+  $scope.schoolname = null;
+  $scope.schoolmascot = null;
+  $scope.gradyear = null;
+  $scope.gradcity = null;
+  $scope.gradstate = null;
+  $scope.picurl = null;
+
+  return profileObj;
+
+}
