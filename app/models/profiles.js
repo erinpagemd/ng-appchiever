e83ee91d4830775838c5ef346beeefa4a4dcbe21
YOUR NAME <erinpagemd@yahoo.com>
2015-03-21 10:25:24 -0500

    synchronize data with firebaseArray

--- app/models/profiles.js
+++ app/models/profiles.js
@@ -4,9 +4,5 @@ angular
   var usersfb = new Firebase(BASE_URL + '/users');
   var fbArray = $firebaseArray(usersfb);
 
-  function save (user) {
-    fbArray.$add(user);
-  }
-
-  return {save:save};
+  return fbArray;
 });
--- app/views/profile.js
+++ app/views/profile.js
@@ -2,7 +2,12 @@ angular
 .module('appchiever')
 .controller('ProfileCtrl', function ($scope, ProfileFactory) {
 
+  //add ProfileFactory to the scope
+  $scope.user = ProfileFactory;
+
+  //save the profile in firebase
   $scope.save = function () {
-    ProfileFactory.save($scope.user);
+    $scope.user.$add($scope.user);
   };
+
 });
