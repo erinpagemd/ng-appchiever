8d014edec3d806b47f4bcbbdb8d05d452309fd15
YOUR NAME <erinpagemd@yahoo.com>
2015-03-22 13:05:47 -0500

    logout user using angularFire method

--- app/views/login.js
+++ app/views/login.js
@@ -5,16 +5,22 @@ angular
 
   $scope.auth.$onAuth(function(authData){
     $scope.authData = authData;
-    console.log('user logged in with authData: ' + authData)
+    console.log('user logged in with authData: ' + authData);
   })
 
   $scope.createUser = function () {
     AuthFactory.$createUser($scope.user)
     .then(function(userData){
+      console.log(AuthFactory);
       console.log("user created with uid: " + userData.uid);
     }).catch(function(error){
       console.log("error creating user: " + error);
     });
+  }
+
+  $scope.unauthUser = function () {
+    AuthFactory.$unauth();
+    console.log('unauth')
   }
 
 });
