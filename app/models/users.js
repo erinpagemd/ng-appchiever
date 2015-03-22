218a86bd3b89fa67e5b91c18a37d0c0ae95a5c7d
YOUR NAME <erinpagemd@yahoo.com>
2015-03-22 12:45:18 -0500

    checks for authData when login page loads

--- app/models/users.js
+++ app/models/users.js
@@ -3,31 +3,4 @@ angular
 .factory('AuthFactory', function (BASE_URL, $firebaseAuth) {
     var fb = new Firebase(BASE_URL);
     return $firebaseAuth(fb);
-
-  // function loginStatus () {
-  //   console.log(fb.getAuth());
-  //   // if (authData) {
-  //   //   console.log('User ' + authData.uid + ' is logged in with ' + authData.provider);
-  //   // } else {
-  //   //   console.log('User is logged out');
-  //   // }
-  // }
-  //
-  // function register (loginObj, cb) {
-  //   fb.createUser(loginObj, cb);
-  //   console.log(fb.getAuth());
-  //
-  // }
-  //
-  // function login (loginObj, cb) {
-  //   fb.authWithPassword(loginObj, cb);
-  //   console.log(fb.getAuth());
-  //
-  // }
-  //
-  // function logout (cb) {
-  //   fb.unauth(cb);
-  // }
-  //
-  // return {register:register, login:login, logout:logout};
 });
--- app/views/login.js
+++ app/views/login.js
@@ -1,6 +1,13 @@
 angular
 .module('appchiever')
 .controller('LoginCtrl', function ($scope, AuthFactory) {
+  $scope.auth = AuthFactory;
+
+  $scope.auth.$onAuth(function(authData){
+    $scope.authData = authData;
+    console.log('user logged in with authData: ' + authData)
+  })
+
   $scope.createUser = function () {
     AuthFactory.$createUser($scope.user)
     .then(function(userData){
@@ -10,29 +17,4 @@ angular
     });
   }
 
-
-  // $scope.getCreds = function () {
-  //   AuthFactory.login($scope.user, function (err, authData) {
-  //     if (err) {
-  //       console.log('Error logging in user: ', err);
-  //     } else {
-  //       console.log('Logged in successfully ', authData);
-  //       //need to grab simpleLogin/uid
-  //     }
-  //   });
-  // };
-  //
-  // $scope.makeAuth = function () {
-  //   AuthFactory.register($scope.user, function (err, authData) {
-  //     if (err && err.code === 'EMAIL_TAKEN') {
-  //       console.log('Error creating user/ logging in: ', err);
-  //       $scope.getCreds();
-  //     } else if (err) {
-  //       console.log('Error creating user: ', err)
-  //     } else {
-  //       console.log('User created successfully', authData);
-  //       $scope.getCreds();
-  //     }
-  //   });
-  // };
 });
