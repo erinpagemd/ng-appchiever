76aff1aaa097b84436056d5287b94d06003f0a15
YOUR NAME <erinpagemd@yahoo.com>
2015-03-22 12:39:38 -0500

    registers user using the AngularFire method

--- app/models/users.js
+++ app/models/users.js
@@ -1,32 +1,33 @@
 angular
 .module('appchiever')
-.factory('AuthFactory', function (BASE_URL) {
-  var fb = new Firebase(BASE_URL);
+.factory('AuthFactory', function (BASE_URL, $firebaseAuth) {
+    var fb = new Firebase(BASE_URL);
+    return $firebaseAuth(fb);
 
-  function loginStatus () {
-    console.log(fb.getAuth());
-    // if (authData) {
-    //   console.log('User ' + authData.uid + ' is logged in with ' + authData.provider);
-    // } else {
-    //   console.log('User is logged out');
-    // }
-  }
-
-  function register (loginObj, cb) {
-    fb.createUser(loginObj, cb);
-    console.log(fb.getAuth());
-
-  }
-
-  function login (loginObj, cb) {
-    fb.authWithPassword(loginObj, cb);
-    console.log(fb.getAuth());
-
-  }
-
-  function logout (cb) {
-    fb.unauth(cb);
-  }
-
-  return {register:register, login:login, logout:logout};
+  // function loginStatus () {
+  //   console.log(fb.getAuth());
+  //   // if (authData) {
+  //   //   console.log('User ' + authData.uid + ' is logged in with ' + authData.provider);
+  //   // } else {
+  //   //   console.log('User is logged out');
+  //   // }
+  // }
+  //
+  // function register (loginObj, cb) {
+  //   fb.createUser(loginObj, cb);
+  //   console.log(fb.getAuth());
+  //
+  // }
+  //
+  // function login (loginObj, cb) {
+  //   fb.authWithPassword(loginObj, cb);
+  //   console.log(fb.getAuth());
+  //
+  // }
+  //
+  // function logout (cb) {
+  //   fb.unauth(cb);
+  // }
+  //
+  // return {register:register, login:login, logout:logout};
 });
--- app/views/login.js
+++ app/views/login.js
@@ -1,29 +1,38 @@
 angular
 .module('appchiever')
-.controller('LoginCtrl', function ($scope, AuthFactory, BASE_URL) {
-
-  $scope.getCreds = function () {
-    AuthFactory.login($scope.user, function (err, authData) {
-      if (err) {
-        console.log('Error logging in user: ', err);
-      } else {
-        console.log('Logged in successfully ', authData);
-        //need to grab simpleLogin/uid
-      }
+.controller('LoginCtrl', function ($scope, AuthFactory) {
+  $scope.createUser = function () {
+    AuthFactory.$createUser($scope.user)
+    .then(function(userData){
+      console.log("user created with uid: " + userData.uid);
+    }).catch(function(error){
+      console.log("error creating user: " + error);
     });
-  };
+  }
 
-  $scope.makeAuth = function () {
-    AuthFactory.register($scope.user, function (err, authData) {
-      if (err && err.code === 'EMAIL_TAKEN') {
-        console.log('Error creating user/ logging in: ', err);
-        $scope.getCreds();
-      } else if (err) {
-        console.log('Error creating user: ', err)
-      } else {
-        console.log('User created successfully', authData);
-        $scope.getCreds();
-      }
-    });
-  };
+
+  // $scope.getCreds = function () {
+  //   AuthFactory.login($scope.user, function (err, authData) {
+  //     if (err) {
+  //       console.log('Error logging in user: ', err);
+  //     } else {
+  //       console.log('Logged in successfully ', authData);
+  //       //need to grab simpleLogin/uid
+  //     }
+  //   });
+  // };
+  //
+  // $scope.makeAuth = function () {
+  //   AuthFactory.register($scope.user, function (err, authData) {
+  //     if (err && err.code === 'EMAIL_TAKEN') {
+  //       console.log('Error creating user/ logging in: ', err);
+  //       $scope.getCreds();
+  //     } else if (err) {
+  //       console.log('Error creating user: ', err)
+  //     } else {
+  //       console.log('User created successfully', authData);
+  //       $scope.getCreds();
+  //     }
+  //   });
+  // };
 });
