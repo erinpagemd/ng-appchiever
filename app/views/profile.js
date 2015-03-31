482b01cc7d46fa2f1eb5439307052fba7acf3d23
YOUR NAME <erinpagemd@yahoo.com>
2015-03-30 21:51:59 -0700

    moved modals to their own file

--- app/views/profile.js
+++ app/views/profile.js
@@ -2,6 +2,10 @@ angular
 .module('appchiever')
 .controller('ProfileCtrl', function ($firebaseObject, $scope, ProfileFactory, ActivityFactory, ClassFactory, $rootScope) {
 
+  $scope.delete = function(){
+    //delete the record
+  }
+
   $scope.update = function(){
     $scope.edituser.$save($scope.edituser);
 
@@ -14,14 +18,18 @@ angular
 
   $scope.hovering = false;
 
+  //add ClassFactory to the scope
   $scope.classes = ClassFactory;
 
+  //add the class to firebase
   $scope.addClass = function() {
     $scope.classes.$add($scope.class);
   }
 
+  //add ActivityFactory to the scope
   $scope.activities = ActivityFactory;
 
+  //add the activity to firebase
   $scope.addActivity = function() {
     $scope.activities.$add($scope.activity);
   }
